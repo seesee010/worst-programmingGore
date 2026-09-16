033d782df9480480eceb946ad2152453b9c1244c
seesee010 <seesee010.kernel@gmail.com>
2026-09-16 11:46:57 +0200

    Add: first wierd program

---
+++ lang/c/wierd-define.seesee010.c
@@ -0,0 +1,19 @@
+#if 0
+seesee010, wierd c
+---
+compile: gcc main.c -o main
+run: ./main
+#endif
+
+#define o int
+#define a main
+#define OA {
+#define CA }
+#define w4 char
+#define we return
+#define sdg4 (
+#define sdg3 )
+#define sdsghi ;
+
+#include <stdio.h>
+o a sdg4 o argc, w4 *argv[] sdg3 OA printf sdg4 "Hello, World!\n" sdg3 sdsghi CA
